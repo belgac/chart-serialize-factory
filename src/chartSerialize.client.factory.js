e7cd3cf62e53e8ac6ec1b36394f9c6876e7144c5
belgac <hans.scheuren@bizliner.com>
2015-11-25 13:54:30 +0000

    feat(factory): add initial factory
    create the base factory

---
+++ src/chartSerialize.client.factory.js
@@ -0,0 +1,25 @@
+angular.module('magicManagerApp.chartSerialize.factory', [])
+  .factory('chartSerializeFactory', chartSerializeFactory);
+  
+  function chartSerializeFactory(){
+    var factory = {};
+    
+    factory.serialize = serialize;
+    
+    return factory;
+    
+    function serialize(data,xfield,yfields){
+      var serializedFields = [];
+      for (var i in yfields){
+        var serializeField = {key:yfields[i].label,values:[]}
+        for (var dataI in data) {
+          var x = data[dataI][xfield.key];
+          var y =  data[dataI][yfields[i].key]
+          var point = {x:x,y:y};
+          serializeField.values.push(point);
+        }
+        serializedFields.push(serializeField);
+      }
+      return serializedFields
+    }
+  }
